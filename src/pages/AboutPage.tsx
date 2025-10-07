15cb995d5a56d2fd244d599e1c813943f882aa40
paveleo <pavel290805@gmail.com>
2025-10-08 01:21:46 +0900

    обновил сайт: новые тексты и сборка

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,6 +4,28 @@ import s from './AboutPage.module.css';
 function AboutText() {
   return (
     <p className={s.lead}>
+      <span className={s.hl}>Мы верим</span>, что мебель — это больше, чем функциональность.
+      Это атмосфера, в которой просыпаешься, строишь планы,
+      <span className={s.hl}> обнимаешь близких</span> и просто живёшь. Именно поэтому каждое
+      изделие мы создаём с <span className={s.hl}>вниманием, любовью</span> и пониманием, что
+      настоящий дом начинается <span className={s.hl}>с деталей</span>.
+      <span className={s.hl}>Мы — динамично развивающаяся компания,</span>специализирующаяся на производстве корпусной мебели, пленочных фасадов, а также реализации материалов и комплектующих. Сегодня мы являемся крупнейшим поставщиком мебельных материалов и фурнитуры в
+      <br />
+      <span className={s.hl}>Республике Саха (Якутия).</span>
+    </p>
+  );
+}
+function AboutText2() {
+  return (
+    <p className={s.lead}>
+      В нашем распоряжении <span className={s.hl}>собственные производственные цеха,</span> оснащённые современными станками и оборудованием. За качество отвечают опытные мастера и проектировщики, что позволяет реализовывать проекты любой сложности и поддерживать <span className={s.hl}>высокие стандарты.</span>
+    </p>
+  );
+}
+function AboutText3() {
+  return (
+    <p className={s.lead}>
+      Мы успешно работаем на рынке более <span className={s.hl}>10 лет</span> и за это время завоевали доверие тысяч клиентов. Наша миссия — создавать комфортные и красивые интерьеры, <span className={s.hl}>принося радость и удовлетворение каждому заказчику.</span>
     </p>
   );
 }
@@ -27,6 +49,40 @@ function Row({ img, reverse }: RowProps) {
   );
 }
 
+function Row2({ img, reverse }: RowProps) {
+  return (
+    <div className={`${s.row} ${reverse ? s.reverse : ''}`}>
+      {!reverse ? (
+        <>
+          <div className={s.textCol}><AboutText2 /></div>
+          <div className={s.mediaCol}><img src={img} alt="" /></div>
+        </>
+      ) : (
+        <>
+          <div className={s.mediaCol}><img src={img} alt="" /></div>
+          <div className={s.textCol}><AboutText2 /></div>
+        </>
+      )}
+    </div>
+  );
+}
+function Row3({ img, reverse }: RowProps) {
+  return (
+    <div className={`${s.row} ${reverse ? s.reverse : ''}`}>
+      {!reverse ? (
+        <>
+          <div className={s.textCol}><AboutText3 /></div>
+          <div className={s.mediaCol}><img src={img} alt="" /></div>
+        </>
+      ) : (
+        <>
+          <div className={s.mediaCol}><img src={img} alt="" /></div>
+          <div className={s.textCol}><AboutText3 /></div>
+        </>
+      )}
+    </div>
+  );
+}
 
 export default function AboutPage() {
   return (
@@ -37,7 +93,14 @@ export default function AboutPage() {
       <Row img={Images.About1} />
 
       {/* 2: фото слева, текст справа */}
+      <Row img={Images.About2} reverse />
 
+      {/* 3: текст слева, фото справа */}
+      <Row img={Images.About3} />
+      <Row2 img={Images.About2} reverse />
+
+      {/* 3: текст слева, фото справа */}
+      <Row3 img={Images.About3} />
     </section>
   );
 }

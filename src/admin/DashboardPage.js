841f21f6bf1f3f13763afdd51cb31fb54193b379
paveleo <pavel290805@gmail.com>
2025-10-20 21:09:06 +0900

    up

--- src/admin/DashboardPage.js
+++
@@ -1,2 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export default function DashboardPage() { return _jsxs("div", { children: [_jsx("h1", { children: "\u0414\u0430\u0448\u0431\u043E\u0440\u0434" }), _jsx("p", { children: "\u0414\u043E\u0431\u0440\u043E \u043F\u043E\u0436\u0430\u043B\u043E\u0432\u0430\u0442\u044C!" })] }); }
